7ae97ae5503163b69f005cc73bb8656461dda257
joshuamanlunas <joshua@joshuamanlunas.com>
2022-11-27 20:13:50 +1300

    updates crypter with sample usage

--- src/core/helpers/crypter.ts
+++ src/core/helpers/crypter.ts
@@ -1,5 +1,7 @@
 const crypto = require('crypto-js')
 
+const key = process.env.CRYPTO_KEY || 'changeMe'
+
 // encrypt string with crypto-js
 export const encrypt = (str: string, key: string) => {
     return crypto.AES.encrypt(str, key).toString()
@@ -9,4 +11,11 @@ export const encrypt = (str: string, key: string) => {
 export const decrypt = (str: string, key: string) => {
     return crypto.AES.decrypt(str, key).toString(crypto.enc.Utf8)
 }
+
+// sample usage
+// let encrypted = encrypt('Hello World', key)
+// console.log(`Encrypted: ${encrypted}`)
+//
+// let decrypted = decrypt(encrypted, key)
+// console.log(`Decrypted: ${decrypted}`)
 
